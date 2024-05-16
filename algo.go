1f6ef8eb33d64dccf1f94ee5e8d9d4e0ee11c874
user <user@mail.com>
2024-05-16 20:36:16 +0200

    REF: move removing algorithm to a separate package

--- algo.go
+++ algo.go
@@ -2,25 +2,12 @@ package main
 
 import (
 	"fmt"
+    "queens/array"
 )
-
-// TODO: poor naming
-// TODO: add a documentation
-func remove(nums []int, item int) []int {
-	index := 0
-
-	for _, value := range nums {
-		if value != item {
-			nums[index] = value
-			index++
-		}
-	}
-	return nums[:index]
-}
 
 func main() {
     // TODO: cover with test
-	array := []int{3, 1, 2, 2, 3, 1, 4, 2}
-	result := remove(array, 2)
+	arr := []int{3, 1, 2, 2, 3, 1, 4, 2}
+	result := array.RemoveArrayElementInplace(arr, 2)
 	fmt.Println(result)
 }
