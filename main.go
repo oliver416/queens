956534c1f4b89314f9c89909644eb5d6893bad28
user <user@mail.com>
2024-12-02 23:34:33 +0100

    ADD: a queen placing function

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
-
 func main() {
 	board := Board{}
 	board.Init(8)
+	// TODO: cover it with tests
+	// board.Place(3,4)
+	board.Place(7, 2)
 	board.Show()
 }
--- queens.go
+++ queens.go
@@ -18,12 +18,50 @@ func (b *Board) Init(size int) {
 	}
 
 	for i := 0; i < size; i++ {
-		b.horizontals = append(b.horizontals, zeroes)
+		copy_zeroes := make([]int, len(zeroes))
+		copy(copy_zeroes, zeroes)
+		b.horizontals = append(b.horizontals, copy_zeroes)
 	}
 }
 
 func (b *Board) Show() {
 	for _, field := range b.horizontals {
 		fmt.Println(field)
+	}
+}
+
+func (b *Board) Place(x int, y int) {
+	// TODO: what if x or y < 0??
+	if x < b.size && y < b.size {
+		for i := 0; i < b.size; i++ {
+			b.horizontals[x][i] = 1
+			b.horizontals[i][y] = 1
+		}
+
+		for i, j := x, y; i >= 0 && j >= 0; {
+			b.horizontals[i][j] = 1
+			i--
+			j--
+		}
+
+		for i, j := x, y; i < b.size && j >= 0; {
+			b.horizontals[i][j] = 1
+			i++
+			j--
+		}
+
+		for i, j := x, y; i >= 0 && j < b.size; {
+			b.horizontals[i][j] = 1
+			i--
+			j++
+		}
+
+		for i, j := x, y; i <= b.size-1 && j <= b.size-1; {
+			b.horizontals[i][j] = 1
+			i++
+			j++
+		}
+
+		b.horizontals[x][y] = 8
 	}
 }
