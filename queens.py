3d244c8399be76e2ae809717d8895165df4d4909
user <user@mail.com>
2024-12-04 00:13:16 +0100

    ADD: Niclaus Wirt's solution

---
+++ queens.py
@@ -0,0 +1,13 @@
+# https://en.wikipedia.org/wiki/Eight_queens_puzzle#Explicit_solutions
+
+def queens(n: int, i: int, a: list, b: list, c: list):
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
+
+
+for solution in queens(8, 0, [], [], []):
+    print(solution)
