b662f74555f5491fd9580a2742146f1169e207bc
user <user@mail.com>
2024-12-23 14:35:05 +0100

    CHG: move API controller to a separate directory

--- app/controllers/gin_controller.go
+++ app/controllers/gin_controller.go
@@ -1,218 +1,212 @@
-package app
+package controllers
 
-//import (
-//"github.com/gin-gonic/gin"
-//swaggerFiles "github.com/swaggo/files"     // swagger embed files
-//ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
-//"net/http"
-//docs "queens/app/docs"
-//"queens/app/entities"
-//"queens/app/interfaces"
-//"strconv"
-//)
+import (
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"     // swagger embed files
+	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
+	"net/http"
+	docs "queens/app/docs"
+	"queens/app/entities"
+	"queens/app/repositories"
+	"strconv"
+)
 
-//// TODO: there is some duplication between the structures User and UserRequest
-//type UserRequest struct {
-//Name string `json:"name"`
-//Age  int    `json:"age"`
-//}
+var DB = []entities.User{
+	{ID: 0, Name: "User1", Age: 10},
+	{ID: 1, Name: "User2", Age: 20},
+	{ID: 2, Name: "User3", Age: 30},
+}
 
-//// TODO: remove this
-//var DB = []entities.User{
-//{ID: 0, Name: "User1", Age: 10},
-//{ID: 1, Name: "User2", Age: 20},
-//{ID: 2, Name: "User3", Age: 30},
-//}
+var Repo = repositories.InMemoryRepository{DB: DB}
 
-//var UserController struct {
-//UserInteractor interfaces.UserInteractorInterface
-//}
+type GinController struct{}
 
-//func (u *UserController) ServerErrorHandler(context *gin.Context) {
-//if r := recover(); r != nil {
-//context.JSON(
-//http.StatusInternalServerError,
-//gin.H{"error": "Internal server error"},
-//)
-//}
-//}
+func (g *GinController) ServerErrorHandler(context *gin.Context) {
+	if r := recover(); r != nil {
+		context.JSON(
+			http.StatusInternalServerError,
+			gin.H{"error": "Internal server error"},
+		)
+	}
+}
 
-//// Ping godoc
-//// @Summary Ping
-//// @Schemes
-//// @Description Test endpoint
-//// @Tags test
-//// @Accept json
-//// @Produce json
-//// @Success 200 {string} Ping
-//// @Router /ping [get]
-//func (u *UserController) handler(context *gin.Context) {
-//context.JSON(
-//http.StatusOK,
-//gin.H{"status": "ok"},
-//)
-//}
+// Ping godoc
+// @Summary Ping
+// @Schemes
+// @Description Test endpoint
+// @Tags test
+// @Accept json
+// @Produce json
+// @Success 200 {string} Ping
+// @Router /ping [get]
+func (g *GinController) handler(context *gin.Context) {
+	context.JSON(
+		http.StatusOK,
+		gin.H{"status": "ok"},
+	)
+}
 
-//// GetUsers godoc
-//// @Summary Get users
-//// @Schemes
-//// @Description Get all users
-//// @Tags users
-//// @Accept json
-//// @Produce json
-//// @Success 200 {array} entities.User
-//// @Router /users [get]
-//func (u *UserController) GetUsers(context *gin.Context) {
-//context.JSON(http.StatusOK, DB)
-//}
+// GetUsers godoc
+// @Summary Get users
+// @Schemes
+// @Description Get all users
+// @Tags users
+// @Accept json
+// @Produce json
+// @Success 200 {array} entities.User
+// @Router /users [get]
+func (g *GinController) GetUsers(context *gin.Context) {
+	users := Repo.GetUsers()
+	context.JSON(http.StatusOK, users)
+}
 
-//// GetUser godoc
-//// @Summary Get user
-//// @Schemes
-//// @Description Get user by ID
-//// @Tags users
-//// @Accept json
-//// @Produce json
-//// @Param id path int true "User ID"
-//// @Success 200 {object} entities.User
-//// @Failure 404
-//// @Failure 500
-//// @Router /users/{id} [get]
-//func (u *UserController) GetUser(context *gin.Context) {
-//defer u.ServerErrorHandler(context)
+// GetUser godoc
+// @Summary Get user
+// @Schemes
+// @Description Get user by ID
+// @Tags users
+// @Accept json
+// @Produce json
+// @Param id path int true "User ID"
+// @Success 200 {object} entities.User
+// @Failure 404
+// @Failure 500
+// @Router /users/{id} [get]
+func (g *GinController) GetUser(context *gin.Context) {
+	defer g.ServerErrorHandler(context)
 
-//id, err := strconv.Atoi(context.Param("id"))
+	id, err := strconv.Atoi(context.Param("id"))
 
-//if err != nil {
-//context.JSON(
-//http.StatusNotFound,
-//gin.H{"error": "User is not found"},
-//)
-//return
-//}
+	if err != nil {
+		context.JSON(
+			http.StatusNotFound,
+			gin.H{"error": "User is not found"},
+		)
+		return
+	}
 
-//u.UserInteractor.GetUser(id)
-//context.JSON(http.StatusOK, user)
-//}
+	user := Repo.GetUserByID(id)
+	context.JSON(http.StatusOK, user)
+}
 
-//// CreateUser godoc
-//// @Summary Create user
-//// @Schemes
-//// @Description Create user
-//// @Tags users
-//// @Accept json
-//// @Produce json
-//// @Param request body UserRequest true "User data"
-//// @Success 201 {object} entities.User
-//// @Failure 400
-//// @Failure 500
-//// @Router /users [post]
-//func (u *UserController) CreateUser(context *gin.Context) {
-//var request UserRequest
+// CreateUser godoc
+// @Summary Create user
+// @Schemes
+// @Description Create user
+// @Tags users
+// @Accept json
+// @Produce json
+// @Param request body repositories.UserRequest true "User data"
+// @Success 201 {object} entities.User
+// @Failure 400
+// @Failure 500
+// @Router /users [post]
+func (g *GinController) CreateUser(context *gin.Context) {
+	var request repositories.UserRequest
 
-//if err := context.BindJSON(&request); err != nil {
-//// TODO: add logging
-//// TODO: standardise error messages
-//context.JSON(
-//http.StatusBadRequest,
-//gin.H{"error": "bad request"},
-//)
-//return
-//}
+	if err := context.BindJSON(&request); err != nil {
+		// TODO: add logging
+		// TODO: standardise error messages
+		context.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": "bad request"},
+		)
+		return
+	}
 
-//u.UserInteractor.CreateUser(id, request)
-//context.JSON(http.StatusCreated, user)
-//}
+	user := Repo.CreateUser(request)
+	context.JSON(http.StatusCreated, user)
+}
 
-//// DeleteUser godoc
-//// @Summary Delete user
-//// @Schemes
-//// @Description Delete user
-//// @Tags users
-//// @Accept json
-//// @Produce json
-//// @Param id path int true "User ID"
-//// @Success 204
-//// @Failure 400
-//// @Failure 500
-//// @Router /users/{id} [delete]
-//func (u *UserController) DeleteUser(context *gin.Context) {
-//defer u.ServerErrorHandler(context)
+// DeleteUser godoc
+// @Summary Delete user
+// @Schemes
+// @Description Delete user
+// @Tags users
+// @Accept json
+// @Produce json
+// @Param id path int true "User ID"
+// @Success 204
+// @Failure 400
+// @Failure 500
+// @Router /users/{id} [delete]
+func (g *GinController) DeleteUser(context *gin.Context) {
+	defer g.ServerErrorHandler(context)
 
-//id, err := strconv.Atoi(context.Param("id"))
+	id, err := strconv.Atoi(context.Param("id"))
 
-//if err != nil {
-//context.JSON(
-//http.StatusBadRequest,
-//gin.H{"error": "bad request"},
-//)
-//return
-//}
+	if err != nil {
+		context.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": "bad request"},
+		)
+		return
+	}
 
-//u.UserInteractor.DeleteUser(id)
-//context.Status(http.StatusNoContent)
-//}
+	Repo.DeleteUser(id)
+	context.Status(http.StatusNoContent)
+}
 
-//// UpdateUser godoc
-//// @Summary Update user
-//// @Schemes
-//// @Description Update user
-//// @Tags users
-//// @Accept json
-//// @Produce json
-//// @Param id path int true "User ID"
-//// @Param request body UserRequest true "User data"
-//// @Success 200 {object} entities.User
-//// @Failure 400
-//// @Failure 500
-//// @Router /users/{id} [patch]
-//func (u *UserController) UpdateUser(context *gin.Context) {
-//defer u.ServerErrorHandler(context)
+// UpdateUser godoc
+// @Summary Update user
+// @Schemes
+// @Description Update user
+// @Tags users
+// @Accept json
+// @Produce json
+// @Param id path int true "User ID"
+// @Param request body repositories.UserRequest true "User data"
+// @Success 200 {object} entities.User
+// @Failure 400
+// @Failure 500
+// @Router /users/{id} [patch]
+func (g *GinController) UpdateUser(context *gin.Context) {
+	defer g.ServerErrorHandler(context)
 
-//var request UserRequest
+	var request repositories.UserRequest
 
-//if err := context.BindJSON(&request); err != nil {
-//// TODO: add logging
-//// TODO: standardise error messages
-//context.JSON(
-//http.StatusBadRequest,
-//gin.H{"error": "bad request"},
-//)
-//return
-//}
+	if err := context.BindJSON(&request); err != nil {
+		// TODO: add logging
+		// TODO: standardise error messages
+		context.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": "bad request"},
+		)
+		return
+	}
 
-//ID, err := strconv.Atoi(context.Param("id"))
+	ID, err := strconv.Atoi(context.Param("id"))
 
-//if err != nil {
-//context.JSON(
-//http.StatusNotFound,
-//gin.H{"error": "User has not found"},
-//)
-//return
-//}
+	if err != nil {
+		context.JSON(
+			http.StatusNotFound,
+			gin.H{"error": "User has not found"},
+		)
+		return
+	}
 
-//u.UserInteractor.UpdateUser(id, request)
-//context.JSON(http.StatusOK, user)
-//}
+	user := Repo.UpdateUser(ID, request)
+	context.JSON(http.StatusOK, user)
+}
 
-//func (u *UserController) Run() {
-//app := gin.Default()
-//basePath := "/api/v1"
-//docs.SwaggerInfo.BasePath = basePath
+func (g *GinController) Run() {
+	app := gin.Default()
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
 
-//group := app.Group(basePath)
-//{
-//group.GET("/ping", handler)
-//group.GET("/users", GetUsers)
-//group.GET("/users/:id", GetUser)
-//group.POST("/users", CreateUser)
-//group.DELETE("/users/:id", DeleteUser)
-//group.PATCH("/users/:id", UpdateUser)
-//}
+	group := app.Group(basePath)
+	{
+		group.GET("/ping", g.handler)
+		group.GET("/users", g.GetUsers)
+		group.GET("/users/:id", g.GetUser)
+		group.POST("/users", g.CreateUser)
+		group.DELETE("/users/:id", g.DeleteUser)
+		group.PATCH("/users/:id", g.UpdateUser)
+	}
 
-//app.GET(
-//"/swagger/*any",
-//ginSwagger.WrapHandler(swaggerFiles.Handler),
-//)
-//app.Run()
-//}
+	app.GET(
+		"/swagger/*any",
+		ginSwagger.WrapHandler(swaggerFiles.Handler),
+	)
+	app.Run()
+}
